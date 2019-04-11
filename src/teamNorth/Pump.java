9806fbe5f38e5c990ec574756538f1e04ae8a5a7
Steven Melville <stevenmelville42@gmail.com>
2019-04-11 14:59:54 -0600

    Added Pump Class

--- src/teamNorth/Pump.java
+++ src/teamNorth/Pump.java
@@ -1,4 +1,15 @@
 package teamNorth;
 
 public class Pump {
+    Tank tank85;
+    Tank tank89;
+
+    Pump(){
+        tank85 = Tank.getTank("85");
+        tank89 = Tank.getTank("89");
+    }
+
+    double pumpFuel(double amount, String type){
+        return amount;
+    }
 }
