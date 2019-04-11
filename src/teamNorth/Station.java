3d20ae277869d45cc8aca2bad8220adf18275cd2
Brandon Isbell <brandonisbell1997@gmail.com>
2019-04-11 15:46:39 -0600

    Basic Station Implementation

--- src/teamNorth/Station.java
+++ src/teamNorth/Station.java
@@ -1,4 +1,30 @@
 package teamNorth;
 
 public class Station {
+    Pump pump1;
+    Pump pump2;
+    Pump pump3;
+    Tank tank85;
+    Tank tank89;
+
+    public Station(){
+        tank85 = Tank.getTank("85");
+        tank89 = Tank.getTank("89");
+        pump1 = new Pump();
+        pump2 = new Pump();
+        pump3 = new Pump();
+    }
+
+    public double checkFuelLevel(){
+
+        return 0;
+    }
+
+    public void fillTank(String Type){
+        
+    }
+
+    public void pumpFuel(double amt, Pump whichPump, String Type){
+
+    }
 }
