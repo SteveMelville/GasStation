b6f141a3c58d181ec1254b928b819f16ff50be19
steven.melville <steven.melville@students.snow.edu>
2019-04-12 09:21:43 -0600

    Added tank members

--- src/teamNorth/Tank.java
+++ src/teamNorth/Tank.java
@@ -1,4 +1,22 @@
 package teamNorth;
 
 public class Tank {
+    static Tank tank85;
+    static Tank tank89;
+
+    private Tank(){
+
+    }
+
+    public static Tank getTank(String name){
+        if(name == "85"){
+            if(tank85 == null) tank85 = new Tank();
+            return tank85;
+        }
+        else if(name == "89"){
+            if(tank89 == null) tank89 = new Tank();
+            return tank89;
+        }
+        else return null;
+    }
 }
