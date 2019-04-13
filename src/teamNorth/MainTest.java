4dbce0b8443fde555c8a33ef61ad67afddc2f168
Steven Melville <stevenmelville42@gmail.com>
2019-04-13 11:04:17 -0600

    Added HandleEmpytyTank base functionality and more pumpFuel functionality

--- src/teamNorth/MainTest.java
+++ src/teamNorth/MainTest.java
@@ -25,4 +25,10 @@ class MainTest {
         assertEquals(12.1,pump.pumpFuel(12.1,"85"));
     }
 
+    @Test
+    void HandleEmptyPumpWorks(){
+        Pump pump = new Pump(1);
+        Tank tank = Tank.getTank("85");
+    }
+
 }
--- src/teamNorth/Pump.java
+++ src/teamNorth/Pump.java
@@ -16,12 +16,31 @@ public class Pump {
     }
 
     public double pumpFuel(double amount, String name){
+        double availableFuel = 0, availableFuel2 = 0;
+        switch (name){
+            case "85":  availableFuel = tank85.requestFuel(amount);
+                        if(availableFuel < amount) return HandleEmptyTank(tank85);
+                        break;
+            case "89":  availableFuel = tank89.requestFuel(amount);
+                        if(availableFuel < amount) return HandleEmptyTank(tank89);
+                        break;
+            case "87":  availableFuel = tank85.requestFuel(amount / 2);
+                        if(availableFuel < amount / 2) return HandleEmptyTank(tank85);
+                        availableFuel2 = tank89.requestFuel(amount / 2);
+                        if(availableFuel2 < amount / 2) return HandleEmptyTank(tank89);
+                        break;
+        }
+
         while(amountPumped < amount){
             amountPumped += pumpSpeed;
         }
 
         amountPumped = 0;
         return amount;
+    }
+
+    public double HandleEmptyTank(Tank tank){
+        return -1;
     }
 
     public int getId() {
