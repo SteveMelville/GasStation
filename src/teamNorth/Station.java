480951d8a6bebb6f134584e04895d18f0acf940c
SirNodnarb <43394345+SirNodnarb@users.noreply.github.com>
2019-04-14 16:41:00 -0600

    Implement TankReorder

--- src/teamNorth/Station.java
+++ src/teamNorth/Station.java
@@ -17,17 +17,17 @@ public class Station {
     public double checkFuelLevel(String name){
         if (name == "85"){
             return tank85.getFuelAmount();
-        } else if (name == "85"){
-            return tank85.getFuelAmount();
+        } else if (name == "89"){
+            return tank89.getFuelAmount();
         }
         return 0;
     }
-
-    public void fillTank(String name){
-        
-    }
-
-    public void pumpFuel(double amt, Pump whichPump, String name){
-
+    
+    public void TankReorder(String name){
+        if (name == "85"){
+            tank85.refuelTank(10000-tank85.getFuelAmount());
+        } else if (name == "89"){
+            tank89.refuelTank(10000-tank89.getFuelAmount());
+        }
     }
 }
