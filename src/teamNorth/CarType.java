91e9aee3d7967b1ee1a9a2e0b726b887018e5bf0
TheDarkeOne <ammonzerkle@gmail.com>
2019-04-19 10:44:44 -0600

    Getting a random car works(I think)

--- src/teamNorth/CarType.java
+++ src/teamNorth/CarType.java
@@ -1,5 +1,12 @@
 package teamNorth;
 
+import java.util.Random;
+
 public enum CarType {
-    Car,PickupTruck,Sedan,SemiTruck,SportsCar,SmallCar
+    Car,PickupTruck,Sedan,SemiTruck,SportsCar,SmallCar;
+
+    public static CarType getRandomCar() {
+        Random random = new Random();
+        return values()[random.nextInt(values().length)];
+    }
 }
